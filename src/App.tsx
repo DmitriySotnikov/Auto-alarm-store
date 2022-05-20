210fabc7e7bd38fb2d4ab42d839f8c0a4ed330cf
DmitriySotnikov <dmitrich.1983.38@gmail.ru>
2022-05-21 01:01:17 +0800

    Create directories.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
 import './App.css';
 
-function App() {
+export const App: FC = () => {
   return (
     <div className="App">
-
+      Работает
     </div>
   );
 }
-
-export default App;
