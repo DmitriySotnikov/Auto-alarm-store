318ba93fc46d7c8904526a04a4d7d41b29c70dd7
DmitriySotnikov <dmitrich.1983.38@gmail.ru>
2022-05-22 23:12:24 +0800

    Work with Header.

--- src/components/blocks/Header/index.tsx
+++ src/components/blocks/Header/index.tsx
@@ -2,6 +2,8 @@ import React, {FC} from 'react';
 import '../../../styles/_header.scss'
 import phone from '../../../assets/icon/phone-1.svg'
 import whatsapp from '../../../assets/icon/whatsapp.svg'
+import location from '../../../assets/icon/location.svg'
+import clock from '../../../assets/icon/clock.svg'
 
 export const Header: FC = () => {
     return (
@@ -22,7 +24,15 @@ export const Header: FC = () => {
                         </div>
                     </div>
                     <div className="header-info">
-
+                        <div className="header-infoItem">
+                            <img className="header-infoIcon" src={location} />
+                            <span className="header-infoText">г. Ангарск, 120-й кв-л, строение 54</span>
+                        </div>
+                        <div className="header-infoItem">
+                            <img className="header-infoIcon" src={clock} />
+                            <span className="header-infoText">Пн - Пт :</span>
+                            <span className="header-infoText">9-00 - 19-00</span>
+                        </div>
                     </div>
                 </div>
             </div>
