769c7c8a19722eecd13c63614fe04e611a637ef0
DmitriySotnikov <dmitrich.1983.38@gmail.ru>
2022-05-25 20:16:44 +0800

    Header button.

--- src/components/blocks/HeaderBottom/index.tsx
+++ src/components/blocks/HeaderBottom/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../../styles/_headerBottom.scss'
 import {CatalogTrigger} from "../../simple/CatalogTrigger";
+import {HeaderButton} from "../../simple/HeaderButton";
 
 export const HeaderBottom = () => {
     return (
         <div className="headerBottom">
             <div className="container">
                 <CatalogTrigger/>
+                <HeaderButton/>
             </div>
         </div>
     );
---
+++ src/components/simple/HeaderButton/index.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react';
+
+export const HeaderButton: FC = () => {
+    return (
+        <div className="HeaderButton">
+
+        </div>
+    );
+};
