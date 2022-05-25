2b0037c50389cc6846d709b43d8a7d62c1db5a36
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-25 21:15:09 +0800

    Header button.

--- src/components/simple/HeaderButton/index.tsx
+++ src/components/simple/HeaderButton/index.tsx
@@ -1,9 +1,10 @@
 import React, {FC} from 'react';
+import {Link} from "react-router-dom";
 
 export const HeaderButton: FC = () => {
     return (
         <div className="HeaderButton">
-
+            <Link to={"#"}/>
         </div>
     );
 };
