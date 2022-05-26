6915559645589e055e65e61ae20ca393466bdedf
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-26 22:37:55 +0800

    Breadcrumb.

--- src/components/simple/Breadcrumb/index.tsx
+++ src/components/simple/Breadcrumb/index.tsx
@@ -1,9 +1,23 @@
 import React, {FC} from 'react';
+import "../../../styles/_breadcrumb.scss"
+import {Link} from "react-router-dom";
+import home from "../../../assets/icon/home.svg"
+import arrow from "../../../assets/icon/right-arrow.svg"
 
 export const Breadcrumb: FC = () => {
     return (
-        <div className="breadcrumbBlock">
-            навигашка
+        <div className="breadcrumbs">
+            <ul className="breadcrumbs-list">
+                <li className="breadcrumbs-item">
+                    <Link className="breadcrumbs-link" to={"#"} title="Главная">
+                        <img className="breadcrumbs-home" src={home}/>
+                    </Link>
+                    <img className="breadcrumbs-arrow" src={arrow}/>
+                </li>
+                <li className="breadcrumbs-item">
+                    <Link className="breadcrumbs-link" to={"#"} title="Главная"/>
+                </li>
+            </ul>
         </div>
     );
 };
