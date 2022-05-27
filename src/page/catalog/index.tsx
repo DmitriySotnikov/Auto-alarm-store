9a5aaa49daf41f964008f84fbaa487681d989525
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-28 00:59:42 +0800

    Start catalog.

--- src/page/catalog/index.tsx
+++ src/page/catalog/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {FC} from 'react';
+import "../../styles/_catalog.scss"
 
-export const Catalog = () => {
+export const Catalog: FC = () => {
     return (
         <div className="catalog">
-
+            <div className="catalog-title">
+                <span className="catalog-titleText">Автосигнализации</span>
+            </div>
         </div>
     );
 };
