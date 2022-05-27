4b52de7cf78d92ddeebe3d560d0aeb79bc33c15b
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-28 00:37:31 +0800

    Breadcrumb.

--- src/components/simple/Breadcrumb/index.tsx
+++ src/components/simple/Breadcrumb/index.tsx
@@ -2,22 +2,25 @@ import React, {FC} from 'react';
 import "../../../styles/_breadcrumb.scss"
 import {Link} from "react-router-dom";
 import home from "../../../assets/icon/home.svg"
-import arrow from "../../../assets/icon/right-arrow.svg"
 
 export const Breadcrumb: FC = () => {
     return (
         <div className="breadcrumbs">
             <ul className="breadcrumbs-list">
                 <li className="breadcrumbs-item">
-                    <Link className="breadcrumbs-link" to={"#"} title="Главная">
+                    <Link className="breadcrumbs-linkHome" to={"#"} title="Главная">
                         <img className="breadcrumbs-home" src={home}/>
                     </Link>
                 </li>
-                <li className="breadcrumbs-item">
-                    <Link className="breadcrumbs-link breadcrumbs-link_pos" to={"#"} title="Главная">
+                <li className="breadcrumbs-item breadcrumbs-item_arrow">
+                    <Link className="breadcrumbs-link" to={"#"} title="Главная">
                         Каталог
                     </Link>
-
+                </li>
+                <li className="breadcrumbs-item breadcrumbs-item_arrow">
+                    <Link className="breadcrumbs-link" to={"#"} title="Автосигнализации">
+                        Автосигнализации
+                    </Link>
                 </li>
             </ul>
         </div>
