2feab4d59e164425f27ed84bca92997144c084c4
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-29 17:39:13 +0800

    Tags.

--- src/components/blocks/Sorter/index.tsx
+++ src/components/blocks/Sorter/index.tsx
@@ -1,9 +1,25 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+import arrow from "../../../assets/icon/arrow.svg"
+import "../../../styles/_sorter.scss"
 
 export const Sorter: FC = () => {
+
+    const [arrowPrice, setArrowPrice] = useState(false)
+    const [arrowPopular, setArrowPopular] = useState(false)
+
     return (
         <div className="sorter">
-            Sorter!
+            <div className="sorter-title">
+                <span className="sorter-titleText">Сортировать по :</span>
+            </div>
+            <div className="sorter-item">
+                <span className="sorter-itemText" onClick={() => {setArrowPrice(!arrowPrice)}}>По цене</span>
+                <img className={arrowPrice ? "sorter-arrow sorter-arrow_down" : "sorter-arrow"} src={arrow}/>
+            </div>
+            <div className="sorter-item">
+                <span className="sorter-itemText" onClick={() => {setArrowPopular(!arrowPopular)}}>По новизне</span>
+                <img className={arrowPopular ? "sorter-arrow sorter-arrow_down" : "sorter-arrow"} src={arrow}/>
+            </div>
         </div>
     );
 };
