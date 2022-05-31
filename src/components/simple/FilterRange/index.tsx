5d8bbf86d2f30b40302a48936218705a1593f185
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-05-31 23:55:51 +0800

    Filters

--- src/components/simple/FilterRange/index.tsx
+++ src/components/simple/FilterRange/index.tsx
@@ -3,28 +3,46 @@ import "../../../styles/_filter-range.scss"
 
 export const FilterRange: FC = () => {
 
-    const [val, setVal] = useState(50)
+    const [minVal, setMinVal] = useState(0)
+    const [maxVal, setMaxVal] = useState(50)
 
     const getInitialState = () => {
         return {value: 3};
     }
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setVal(() => Number(event.target.value))
-        console.log(event.target.value)
+    const handleChangeMin = (event: ChangeEvent<HTMLInputElement>) => {
+        setMinVal(() => Number(event.target.value))
+        //console.log(event.target.value)
+    }
+    const handleChangeMax = (event: ChangeEvent<HTMLInputElement>) => {
+        setMaxVal(() => Number(event.target.value))
+        //console.log(event.target.value)
+    }
+    const handleChangeDiv = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target)
     }
     return (
-        <div className="filterRange">
+        <div className="filterRange" onChange={handleChangeDiv}>
             <input className="filterRange-input"
-                id="typeinp"
-                type="range"
-                min="0"
-                max="50"
-                value={val}
-                onChange={handleChange}
-                step="1"
+                   id="typeinp"
+                   type="range"
+                   min="0"
+                   max="50"
+                   value={minVal}
+                   onChange={handleChangeMin}
+                   step="1"
+                   onClick={() => {console.log("клик")}}
             />
-
+            <input className="filterRange-input"
+                   id="typeinp"
+                   type="range"
+                   min="0"
+                   max="50"
+                   value={maxVal}
+                   onChange={handleChangeMax}
+                   step="1"
+                   onClick={() => {console.log("клик")}}
+            />
         </div>
     );
 };
