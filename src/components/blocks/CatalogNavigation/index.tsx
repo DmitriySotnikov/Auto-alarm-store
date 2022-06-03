557802664f6ac41cfb99e2fff7c8fd785dbecee7
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-03 21:23:00 +0800

    Work with filter content.

--- src/components/blocks/CatalogNavigation/index.tsx
+++ src/components/blocks/CatalogNavigation/index.tsx
@@ -1,32 +1,18 @@
 import React, {FC} from 'react';
-import {catalogLink} from "../../../util/catalogLink"
 import "../../../styles/_catalogNavigation.scss"
-import {Link} from "react-router-dom";
 import {FilterRange} from "../../simple/FilterRange";
 import {CheckBoxField} from "../../simple/FilterCheckBox";
+import {NavigationItems} from "../../simple/NavigationItem";
 
 export const CatalogNavigation: FC = () => {
-
-    const flag = false
-
     return (
         <div className="catalogNavigation">
-            <nav className={flag ?
-                "catalogNavigation-nav"
-                : "catalogNavigation-nav catalogNavigation-nav_filter"}>
-                <FilterRange/>
-                <CheckBoxField/>
-                {flag && catalogLink.map(e => {
-                    return (
-                        <div className="catalogNavigation-item">
-                            <Link className="catalogNavigation-link"
-                                  to={e.link}
-                            >{e.textLink}
-                            </Link>
-                        </div>
-                    )
-                })}
+            <nav className="catalogNavigation-nav">
+                <NavigationItems/>
             </nav>
         </div>
     );
 };
+
+//<FilterRange/>
+//<CheckBoxField/>
--- src/components/simple/FilterCheckBox/index.tsx
+++ src/components/simple/FilterCheckBox/index.tsx
@@ -1,11 +1,20 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC} from 'react';
 import "../../../styles/_checkbox.scss"
 
 export const CheckBoxField: FC = () => {
+
+    const handler = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.checked)
+        console.log(event.target.value)
+    }
+
     return (
         <div className="checkbox">
-            <label>
-                <input className="checkbox-input" type={"checkbox"}/>
+            <label className="checkbox-label">
+                <input className="checkbox-input"
+                       type="checkbox"
+                       onChange={handler}
+                       />
                 <span className="checkbox-icon"/>
                 <span className="checkbox-textTitle">С автозапуском</span>
             </label>
