3ede565cd35e82d8647c5e95c569e5b12ad5a24e
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-05 23:30:43 +0800

    Catalog item

--- src/components/blocks/Tags/index.tsx
+++ src/components/blocks/Tags/index.tsx
@@ -5,29 +5,29 @@ import {Link} from "react-router-dom";
 export const Tags: FC = () => {
     return (
         <div className="tags">
-            <div className="tags-item">
-                <Link className="tags-link" to={"#"}>
+            <div className="tags__item">
+                <Link className="tags__link" to={"#"}>
                     Pandora
                 </Link>
             </div>
-            <div className="tags-item">
-                <Link className="tags-link" to={"#"}>
+            <div className="tags__item">
+                <Link className="tags__link" to={"#"}>
                     StarLine
                 </Link>
             </div>
-            <div className="tags-item">
-                <Link className="tags-link" to={"#"}>
+            <div className="tags__item">
+                <Link className="tags__link" to={"#"}>
                     Mongoose
                 </Link>
             </div>
-            <div className="tags-item">
-                <Link className="tags-link" to={"#"}>
-                    С запуском
+            <div className="tags__item">
+                <Link className="tags__link" to={"#"}>
+                    Tomahawk
                 </Link>
             </div>
-            <div className="tags-item">
-                <Link className="tags-link" to={"#"}>
-                    Без брелка
+            <div className="tags__item">
+                <Link className="tags__link" to={"#"}>
+                    Sheriff
                 </Link>
             </div>
         </div>
