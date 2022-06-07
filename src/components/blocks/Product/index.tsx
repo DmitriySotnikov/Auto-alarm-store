aac8aed7cea400974bdbac662c04490240e03d5b
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-08 00:21:59 +0800

    Product card

--- src/components/blocks/Product/index.tsx
+++ src/components/blocks/Product/index.tsx
@@ -6,6 +6,11 @@ export const Product: FC = () => {
         <div className="prd">
             <div className="prd__header">
                 <h1 className="prd__header-text">Pandora DX 4GL plus</h1>
+                <div className="prd__header-rating">
+                    <span className="prd__header-rating-stars-line product-header-rating-stars-line--4"/>
+                    <span className="prd__header-rating-count">5 баллов</span>
+                    <span>( 10 голосов )</span>
+                </div>
             </div>
 
         </div>
