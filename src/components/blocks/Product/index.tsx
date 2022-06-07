caa8515e02a884b39721fd2f52d3a955cea4ccda
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-08 00:33:32 +0800

    Product card

--- src/components/blocks/Product/index.tsx
+++ src/components/blocks/Product/index.tsx
@@ -1,15 +1,19 @@
 import React, {FC} from 'react';
 import "../../../styles/_prd.scss"
+import comments from "../../../assets/icon/comment.svg"
 
 export const Product: FC = () => {
     return (
         <div className="prd">
             <div className="prd__header">
                 <h1 className="prd__header-text">Pandora DX 4GL plus</h1>
-                <div className="prd__header-rating">
-                    <span className="prd__header-rating-stars-line product-header-rating-stars-line--4"/>
-                    <span className="prd__header-rating-count">5 баллов</span>
-                    <span>( 10 голосов )</span>
+                <div className="prd__rating-reviews">
+                    <span className="prd__rating-stars-line product-rating-stars-line--4"/>
+                    <span className="prd__rating-count">5 баллов</span>
+                    <span className="prd__reviews-count">( 10 голосов )</span>
+                </div>
+                <div>
+                    <img src={comments}/>
                 </div>
             </div>
 
