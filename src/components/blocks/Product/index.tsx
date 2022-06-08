c34a0e2dfa9422e3861273eb1af629a774fbc811
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-08 23:11:46 +0800

    Product card

--- src/components/blocks/Product/index.tsx
+++ src/components/blocks/Product/index.tsx
@@ -22,6 +22,51 @@ export const Product: FC = () => {
                 <div className="prd__price-wrap">
                     <span className="prd__price">34 059 ₽</span>
                 </div>
+                <div className="prd__attributes">
+                    <ul className="prd__list">
+                        <li className="prd__list-item">
+                            Способы управления :
+                            <ul className="prd__list">
+                                <li className="prd__list-item">Bluetooth Smart - смартфон</li>
+                                <li className="prd__list-item">Бесконтактная метка</li>
+                                <li className="prd__list-item">Штатный ключ от автомобиля (Slave)</li>
+                                <li className="prd__list-item">Штатные кнопки в салоне</li>
+                                <li className="prd__list-item">Мобильный телефон (GSM)</li>
+                                <li className="prd__list-item">Мобильное приложение</li>
+                            </ul>
+                        </li>
+                        <li className="prd__list-item">
+                            Автозапуск
+                        <span className="prd__item-value">
+                            есть
+                        </span>
+                        </li>
+                        <li className="prd__list-item">
+                            Тип GSM-модуля
+                        <span className="prd__item-value">
+                            4G
+                        </span>
+                        </li>
+                        <li className="prd__list-item">
+                            Поддержка Pandora CLONE
+                        <span className="prd__item-value">
+                            есть
+                        </span>
+                        </li>
+                        <li className="prd__list-item">
+                            Bluetooth Smart
+                        <span className="prd__item-value">
+                            есть
+                        </span>
+                        </li>
+                        <li className="prd__list-item">
+                            Версия Bluetooth
+                        <span className="prd__item-value">
+                            4.2
+                        </span>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
         </div>
