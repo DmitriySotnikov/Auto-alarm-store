bfe4afb1825052ed3221ac6b031ee04f89fb8a78
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-11 22:48:00 +0800

    Cart

--- src/components/blocks/Cart/index.tsx
+++ src/components/blocks/Cart/index.tsx
@@ -3,6 +3,8 @@ import "../../../styles/cart.scss"
 import alarm from "../../../assets/icon/ux4150.jpg"
 import trashCan from "../../../assets/icon/trash-can.svg"
 import heart from "../../../assets/icon/heart-grey.svg"
+import plus from "../../../assets/icon/plus.svg"
+import minus from "../../../assets/icon/minus.svg"
 
 export const Cart: FC = () => {
     return (
@@ -11,40 +13,45 @@ export const Cart: FC = () => {
                 <h1 className="cart__title-text">Корзина</h1>
                 <div className="cart__prd-count">2 товара</div>
             </div>
-            <div className="cart__box">
-                <div className="cart__box-pic">
-                    <img className="cart__box-img" src={alarm}/>
-                </div>
-                <div className="cart__prd-name">
-                    <h2>Автосигнализация Pandora DXL-90 LORA</h2>
-                    <div className="cart__ptr-art">
-                        <span className="cart__ptr-art-prefix">артикул</span>
-                        <span className="cart__ptr-art-number">123456789</span>
+            <div className="cart__content">
+                <div className="cart__box">
+                    <div className="cart__box-pic">
+                        <img className="cart__box-img" src={alarm}/>
                     </div>
-                </div>
-                <div className="cart__box-price-count">
-                    <div>
-                        <span>23 700 ₽</span>
-                    </div>
-                    <div className="cart__box-prd-count">
-                        <div className="cart__box-prd-count-button">
-                            +
-                        </div>
-                        <div className="cart__prd-count">
-                            <span className="cart__prd-count-number">1</span>
-                        </div>
-                        <div className="cart__box-prd-count-button">
-                            -
+                    <div className="cart__prd-name">
+                        <h2 className="cart__prd-name-text">Автосигнализация Pandora DXL-90 LORA</h2>
+                        <div className="cart__ptr-art">
+                            <span className="cart__ptr-art-prefix">артикул</span>
+                            <span className="cart__ptr-art-number">123456789</span>
                         </div>
                     </div>
+                    <div className="cart__box-price-count">
+                        <div className="cart__box-price">
+                            <span className="cart__price">23 700 ₽</span>
+                        </div>
+                        <div className="cart__count-block">
+                            <div className="cart__count-block-button">
+                                <img src={plus}/>
+                            </div>
+                            <div className="cart__count-block-count">
+                                <span className="cart__count-block-number">1</span>
+                            </div>
+                            <div className="cart__count-block-button">
+                                <img src={minus}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cart__box-end-block">
+                        <div className="cart__box-in-favorites">
+                            <img className="cart__end-block-img" src={heart}/>
+                        </div>
+                        <div className="cart__box-remove">
+                            <img className="cart__end-block-img" src={trashCan}/>
+                        </div>
+                    </div>
                 </div>
-                <div className="cart__box-end-block">
-                    <div className="cart__box-in-favorites">
-                        <img src={heart}/>
-                    </div>
-                    <div className="cart__box-remove">
-                        <img src={trashCan}/>
-                    </div>
+                <div className="cart__check">
+
                 </div>
             </div>
         </div>
