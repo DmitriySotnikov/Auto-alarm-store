38831b948b9a8e181d4a84b5acd8ce9b38e1ea3a
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-19 00:03:12 +0800

    Getting started with the footer component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "../../pages/HomePage";
 
 import "../../styles/_index.scss"
 import {Layout} from "../Layout";
+import {Footer} from "../Footer";
 
 export const App: FC = () => {
     return (
@@ -12,6 +13,7 @@ export const App: FC = () => {
             <Layout>
                 <Home/>
             </Layout>
+            <Footer/>
         </div>
     );
 }
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {SliderAuto} from "../../components/CarouselAutoPlay/Slider";
 import {PrdCardSmall} from "../../components/ProductCardSmall";
 import {Banners} from "../../components/Banners";
+import {Footer} from "../../components/Footer";
 
 export const Home: FC = () => {
     return (
