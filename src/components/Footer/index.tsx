ed8acbcc1ab1709c4da994db2524d7b45166c4e4
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-06-19 23:47:00 +0800

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from 'react';
+import {IconSprite} from "../IconSprite";
+import {Link} from "react-router-dom";
 
 export const Footer: FC = () => {
     return (
         <div className="footer">
             <div className="footer__container">
-                <div className="footer__menu">
+                <div className="footer__inner">
                     <div className="footer__contacts">
                         <div className="footer__title">
                             Контакты :
@@ -35,25 +37,52 @@ export const Footer: FC = () => {
                         </div>
                     </div>
                     <div className="footer__info-block">
-                    <div className="footer__title">
-                        Мы в социальных сетях и мессежерах:
+                        <div className="footer__title">
+                            Мы в социальных сетях и мессежерах:
+                        </div>
+                        <div className="footer__img-list">
+                            <span className="footer__vk-icon footer--indent"/>
+                            <span className="footer__whatsapp-icon footer--indent"/>
+                            <span className="footer__telegram-icon"/>
+                        </div>
+                        <div className="footer__title">
+                            Принимаем к оплате:
+                        </div>
+                        <div className="footer__img-list">
+                            <svg className="footer__mastercard-icon footer--indent">
+                                <use xlinkHref="#mastercard"/>
+                            </svg>
+                            <svg className="footer__mir-icon footer--indent">
+                                <use xlinkHref="#mir"/>
+                            </svg>
+                            <svg className="footer__visa-icon">
+                                <use xlinkHref="#visa"/>
+                            </svg>
+                        </div>
                     </div>
-                    <div className="footer__img-list">
-                        <span className="footer__vk-icon"/>
-                        <span className="footer__whatsapp-icon"/>
-                        <span className="footer__telegram-icon"/>
+                    <div className="footer__menu">
+                        <div className="footer__title">
+                            Разделы :
+                        </div>
+                        <div className="footer__menu-list-item">
+                            <Link className="footer__menu-list-link" to={"#"}>
+                                О нас
+                            </Link>
+                        </div>
+                        <div className="footer__menu-list-item">
+                            <Link className="footer__menu-list-link" to={"#"}>
+                                Услуги
+                            </Link>
+                        </div>
+                        <div className="footer__menu-list-item">
+                            <Link className="footer__menu-list-link" to={"#"}>
+                                Правовая информация
+                            </Link>
+                        </div>
                     </div>
-                    <div className="footer__title">
-                        Принимаем к оплате:
-                    </div>
-                    <div className="footer__img-list">
-                        <span className="footer__vk-icon"/>
-                        <span className="footer__whatsapp-icon"/>
-                        <span className="footer__telegram-icon"/>
-                    </div>
-                </div>
                 </div>
             </div>
+            <IconSprite/>
         </div>
     );
 };
